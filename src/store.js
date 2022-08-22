426d22d3fa1c2efeb978c65997a8b545a834510c
wang-hsoo <dhkd4873@naver.com>
2022-08-22 17:04:41 +0900

    redux tollkit

--- src/store.js
+++ src/store.js
@@ -1,40 +1,37 @@
 import {createStore} from "redux";
+import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = (text) => {
-    return{
-        type: ADD,
-        text
-    };
-};
-
-const deleteToDo = (id) => {
-    return{
-        type: DELETE,
-        id : parseInt(id)
-    };
-};
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE")
 
 const getToDo = localStorage.getItem("toDo");
 
-const reducer = (state = JSON.parse(getToDo) || [], action) => {
+/*const reducer = (state = JSON.parse(getToDo) || [], action) => {
     switch(action.type){
-        case ADD:
-            const toDo = [{text: action.text, id: Date.now()}, ...state]
+        case addToDo.type:
+            const toDo = [{text: action.payload, id: Date.now()}, ...state]
             localStorage.setItem("toDo", JSON.stringify(toDo));
             return toDo;
-        case DELETE:
-            const todo = state.filter(toDo => toDo.id !== action.id);
+        case deleteToDo.type:
+            const todo = state.filter(toDo => toDo.id !== action.payload);
             localStorage.setItem("toDo", JSON.stringify(todo));
             return todo;
         default:
             return state;
     }
-}
+}*/
 
-const store = createStore(reducer);
+const reducer = createReducer([], {
+    // state argument 를 mutate하거나 new state를 리턴해야 한다
+    [addToDo]: (state, action) => {
+        state.push({text: action.payload, id: Date.now()});
+    },
+    [deleteToDo]: (state, action) => 
+        state.filter(toDo => toDo.id !== action.payload)
+    
+})
+
+const store = configureStore({reducer});
 
 export const actionCreators = {
     addToDo,
